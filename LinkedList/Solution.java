db29e89c4c735cb109393a357fdf496839dcf6c1
muhammadanascode <anassohal34343@gmail.com>
2024-07-09 16:03:27 +0500

    Problem : to delete duplicates nodes from sorted list

--- LinkedList/Solution.java
+++ LinkedList/Solution.java
@@ -98,6 +98,26 @@ public class Solution {
         return evenHead.next; // Head of the new list
     }
 
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head ;
+        }
+        ListNode prev = head ;
+        ListNode current = head.next ;
+
+        while (current != null) {
+            if (prev.val == current.val) {
+                prev.next = current.next ;
+                
+            }else{
+               prev = current ;
+            }
+            current = current.next ;
+            ;
+        }
+        return head ;
+    }
+
     public static void main(String[] args) {
         Solution sol = new Solution() ;
         SinglyLinkedList sll =new SinglyLinkedList() ;
