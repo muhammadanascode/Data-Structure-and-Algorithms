c4b9803cbeaaaefae0a80654cfb6beee3b4a6e29
muhammadanascode <anassohal34343@gmail.com>
2024-08-15 11:38:35 +0500

    Solution of leetCode Probllems

--- LinkedList/Solution.java
+++ LinkedList/Solution.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.util.List;
+
 import LinkedList.SinglyLinkedList.ListNode;
 
 //Method to check whether the list is palindrome or not
@@ -78,7 +80,7 @@ public class Solution {
         ListNode odd = oddHead;
         ListNode even = evenHead;
         ListNode current = head;
-        int count = 0 ;
+        int count = 0;
 
         while (current != null) {
             if (count % 2 != 0) { // Odd
@@ -89,7 +91,7 @@ public class Solution {
                 even = even.next;
             }
             current = current.next;
-            count++ ;
+            count++;
         }
 
         odd.next = null; // End of even list
@@ -100,31 +102,142 @@ public class Solution {
 
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) {
-            return head ;
+            return head;
         }
-        ListNode prev = head ;
-        ListNode current = head.next ;
+        ListNode prev = head;
+        ListNode current = head.next;
 
         while (current != null) {
-            if (prev.val == current.val) {
-                prev.next = current.next ;
-                
-            }else{
-               prev = current ;
+            if (prev.data == current.data) {
+                prev.next = current.next;
+
+            } else {
+                prev = current;
             }
-            current = current.next ;
+            current = current.next;
             ;
         }
+        return head;
+    }
+
+    public static ListNode mergeTwoSortedList(ListNode x, ListNode y) {
+        ListNode temp = new ListNode(0);
+        ListNode tail = temp;
+
+        while (x != null && y != null) {
+            if (x.data <= y.data) {
+                tail.next = x;
+                x = x.next;
+            } else {
+                tail.next = y;
+                y = y.next;
+            }
+            tail = tail.next;
+        }
+        if (x == null) {
+            tail.next = y;
+        } else {
+            tail.next = x;
+        }
+        return temp.next;
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode newNode = new ListNode(0);
+        ListNode current = newNode;
+
+        while (head != null) {
+            if (head.data != val) {
+                ListNode temp = new ListNode(head.data);
+                current.next = temp;
+                current = current.next;
+            }
+            head = head.next;
+        }
+        return newNode.next;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode newNode = new ListNode(0);
+        ListNode current = newNode;
+        int sum = 0;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int data1 = (l1 != null) ? l1.data : 0;
+            int data2 = (l2 != null) ? l2.data : 0;
+            sum = data1 + data2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            ListNode temp = new ListNode(sum);
+            current.next = temp;
+            current = current.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return newNode.next;
+
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) {
+            return null ;
+        }
+        ListNode lengthFinder = head ;
+        int length = 0 ;
+        while (lengthFinder != null) {
+            length++ ;
+            lengthFinder = lengthFinder.next ;
+        }
+        System.out.println(length);
+        if (n == length) {
+            return head.next ;
+        }
+        ListNode current = head.next ;
+        ListNode prev = head ;
+
+        for(int i= 1 ; i<=length-n-1 ; i++){
+            current = current.next ;
+            prev = prev.next ;
+        }
+        prev.next = current.next ; 
+
         return head ;
     }
 
+    public ListNode reverseList(ListNode head) {
+     if (head == null || head.next == null) {
+        return head ;
+     }  
+     ListNode current = head ;
+     ListNode prev = null ; 
+     while (current != null) {
+        ListNode next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+     }
+     return prev ;
+    }
+
     public static void main(String[] args) {
-        Solution sol = new Solution() ;
-        SinglyLinkedList sll =new SinglyLinkedList() ;
-        sll.insertAtEnd(2);
+        Solution sol = new Solution();
+        SinglyLinkedList sll = new SinglyLinkedList();
+        SinglyLinkedList sll2 = new SinglyLinkedList();
         sll.insertAtEnd(1);
+        sll.insertAtEnd(2);
         sll.insertAtEnd(3);
         sll.insertAtEnd(4);
-       System.out.println(sol.oddEvenList(sll.getHead()).next.next); 
+        sll.insertAtEnd(5);
+        System.out.println(sol.reverse(sll.getHead()));
     }
 }
