e28dc83d24627fedd55f3a382297f85c0fc49535
muhammadanascode <anassohal34343@gmail.com>
2024-08-15 14:40:45 +0500

    Validating parenthesis using stack

---
+++ ValidParennthesis.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class ValidParennthesis {
+    static boolean isValid(String str) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    int top = stack.peek();
+                    if ((c == ')' && top == '(') || (c == '}' && top == '{')
+                            || (c == ']' && top == '[')) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("{({[[}]})}"));
+    }
+}
