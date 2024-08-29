88410dbd1aea1fed5137dbb1441c8197f45e87c8
muhammadanascode <anassohal34343@gmail.com>
2024-08-29 23:04:56 +0500

    Implemented Binary tree added two methods for traversal

---
+++ Trees/BinaryTree.java
@@ -0,0 +1,40 @@
+package Trees;
+
+public class BinaryTree {
+    TreeNode root;
+
+    private class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public void preOrder(TreeNode root){
+        if (root == null) {
+            return ;
+        }
+        System.out.println(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public void inOrder(TreeNode root){
+        if (root == null) {
+            return ;
+        }
+        preOrder(root.left);
+        System.out.println(root.data);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree() ;
+    }
+
+}
