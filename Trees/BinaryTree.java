a63eeb6a5e1bac31aaa8f5109b36b14f4ef30986
muhammadanascode <anassohal34343@gmail.com>
2024-08-30 12:16:32 +0500

    Finding maximum element in binary Tree

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -134,6 +134,22 @@ public class BinaryTree {
         }
     }
 
+    public int findMax(TreeNode root){
+if (root == null) {
+    return -1;
+}
+int result = root.data ;
+int left = findMax(root.left);
+int right = findMax(root.right);
+
+if (left>result) {
+    result = left ;
+}if (right>result) {
+    result = right ;
+}
+return result ;
+    }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
     }
