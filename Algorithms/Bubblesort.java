28fa56c273c4058175bba5b90389753ea16cbf7f
muhammadanascode <anassohal34343@gmail.com>
2024-09-13 20:11:11 +0500

    Bubble sort algorithm created

---
+++ Algorithms/Bubblesort.java
@@ -0,0 +1,28 @@
+package Algorithms;
+
+public class Bubblesort {
+
+    static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length - i -1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 1, 4, 3, 6, 8, 7 };
+        bubbleSort(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+        System.out.println(arr[6]);
+    }
+}
