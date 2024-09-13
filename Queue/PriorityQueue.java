8f3f30fd41cdc3f17e973c975dd69821b4f49028
muhammadanascode <anassohal34343@gmail.com>
2024-09-13 16:45:13 +0500

    Created a method to insert element in binary heap and restore its property after inserting an element

--- Queue/PriorityQueue.java
+++ Queue/PriorityQueue.java
@@ -18,6 +18,25 @@ public class PriorityQueue {
         return n;
     }
 
+    void insert(int val) {
+        if (n == heap.length - 1) {
+            System.out.println("Heap out of memory");
+            return;
+        }
+        heap[++n] = val;
+        swim(n);
+    }
+
+    // restoring the property of binary heap after inserting an element
+    private void swim(int k) {
+        while (k > 1 && heap[k / 2] < heap[k]) {
+            int temp = heap[k];
+            heap[k] = heap[k / 2];
+            heap[k / 2] = temp;
+            k = k / 2;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
