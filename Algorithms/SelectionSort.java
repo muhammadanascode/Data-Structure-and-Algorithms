7ab58f7cd4e7d315aa29128300fd4fce5e30492e
muhammadanascode <anassohal34343@gmail.com>
2024-09-14 16:32:27 +0500

    Bug fixed

--- Algorithms/SelectionSort.java
+++ Algorithms/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
     static void selectionsort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int min = i;
-            for (int j = i + 1; j < arr.length - 1; j++) {
+            for (int j = i + 1; j < arr.length ; j++) {
                 if (arr[min] > arr[j]) {
                     min = j ;
                 }
