46fca0606761f2797c4e74c92a07490d40b1a3f9
muhammadanascode <anassohal34343@gmail.com>
2024-09-14 16:29:16 +0500

    Selection sort algorithm

---
+++ Algorithms/SelectionSort.java
@@ -0,0 +1,26 @@
+package Algorithms;
+
+public class SelectionSort {
+
+    static void selectionsort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j ;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 1, 4, 2, 5 };
+        selectionsort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
